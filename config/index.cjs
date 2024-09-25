4a802307e9555af07aec473001eb3242031fc618
HUYI999 <hugh.yi.88@gmail.com>
2024-09-25 17:21:02 +1000

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oh2Fp6oHyJ5lGwFSKaLvkXL2zWeE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '59yXTzvSnNw22AmqYbhDozcl6RenRDZx28jkP-VaQVM',
+      useTemplateId: 'mlWWaDoxztxxRnXfZBzMItzRGhKabZgodUR_HA_AWn8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-10',
       festivals: [
